620664a5972a8fd458fbe48ba90a86b80d7f5a25
Adam Pietrzycki <adam@pietrzycki.com>
2025-05-16 01:00:50 +0100

    Clean up

--- flake.nix
+++ flake.nix
@@ -44,17 +44,22 @@
       ];
 
       environment.systemPackages = with pkgs; [
-        vim
-        ripgrep
-        watch
-        tldr
-        dive
-        cosign
-        neofetch
         alejandra
+        cosign
+        curl
+        dive
+        ghorg
+        git
+        gnupg
+        neofetch
+        ripgrep
+        starship
+        tldr
+        vim
+        watch
 
         unstable.claude-code
-        unstable.jetbrains.idea-ultimate
+        unstable.mise
       ];
 
       homebrew = {
@@ -62,16 +67,31 @@
         taps = [];
         brews = [];
         casks = [
-          "ghostty"
           "1password"
           "1password-cli"
+          "beyond-compare"
+          "cleanshot"
+          "cursor"
+          "daisydisk"
+          "datagrip"
+          "ghostty"
+          "goland"
+          "intellij-idea"
+          "little-snitch"
+          "lm-studio"
+          "micro-snitch"
+          "orbstack"
+          "proxyman"
+          "pycharm"
+          "slack"
+          "superwhisper"
           "tower"
         ];
 
         masApps = {
           "1Password for Safari" = 1569813296;
-          "Kagi for Safari" = 1622835804;
           "Flighty" = 1358823008;
+          "Kagi for Safari" = 1622835804;
           "Wipr 2" = 1662217862;
         };
       };
@@ -82,8 +102,36 @@
       system.stateVersion = 5;
       nixpkgs.hostPlatform = system;
 
+      fonts.packages = [];
+
+      # Security
       security.pam.enableSudoTouchIdAuth = true;
+      system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
+      # Dock
+      system.defaults.dock.autohide = true;
+      system.defaults.dock.autohide-time-modifier = 0.4;
+      system.defaults.dock.show-recents = false;
+      system.defaults.dock.tilesize = 30;
+
+      # Finder
+      system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+      system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
+      system.defaults.finder.FXDefaultSearchScope = "SCcf";
+      system.defaults.finder.FXRemoveOldTrashItems = true;
+      system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+
+      # Mission Control
+      system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
+      system.defaults.dock.mru-spaces = false;
+
+      # Trackpad
+      system.defaults.trackpad.Clicking = true;
       system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
+      system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+
+      # Windows
+      system.defaults.WindowManager.EnableTiledWindowMargins = false;
     };
   in {
     darwinConfigurations."Adams-MacBook-Pro" = nix-darwin.lib.darwinSystem {
