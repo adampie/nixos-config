94f54fc13bf61396f17f78e4f45f72418ca29ad6
Adam Pietrzycki <adam@pietrzycki.com>
2025-05-16 18:17:47 +0100

    "Enable unfree packages and add overlays for nixpkgs configurations"

--- hosts/personal-mac.nix
+++ hosts/personal-mac.nix
@@ -5,12 +5,26 @@
 }: {
   darwinConfig = inputs.nix-darwin.lib.darwinSystem {
     inherit system;
+    specialArgs = { inherit inputs; };
     modules = [
       ../modules/shared
       ../modules/darwin
       ../modules/profiles/personal
       {
-        nixpkgs.hostPlatform = system;
+        nixpkgs = {
+          hostPlatform = system;
+          config = {
+            allowUnfree = true;
+          };
+          overlays = [
+            (final: prev: {
+              unstable = import inputs.nixpkgs-unstable {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            })
+          ];
+        };
         system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
       }
     ];
