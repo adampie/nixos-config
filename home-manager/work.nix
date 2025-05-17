daa0484257f31beb8c65da92e045cf27d2f3deb5
Adam Pietrzycki <adam@pietrzycki.com>
2025-05-17 21:42:45 +0100

    Refactor Home Manager configuration to use shared modules for personal and work setups. Introduced shared.nix for common settings and streamlined imports in personal and work configurations.

--- home-manager/work.nix
+++ home-manager/work.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
-  home = {
-    stateVersion = "24.11";
-    homeDirectory = builtins.toPath "/Users/adampie";
-    username = "adampie";
-    packages = with pkgs; [
-      dive
-      pkgs.unstable.claude-code
-    ];
-  };
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./shared.nix
+  ];
+
+  home.packages = with pkgs; [
+    dive
+  ];
+
+  programs = {};
 }
--- hosts/work-mac.nix
+++ hosts/work-mac.nix
@@ -3,46 +3,23 @@
   system,
   ...
 }: let
-  pkgs = import inputs.nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-    overlays = [
-      (final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      })
-    ];
-  };
+  shared = import ./shared.nix {inherit inputs system;};
 in
-  inputs.nix-darwin.lib.darwinSystem {
-    inherit system;
-    specialArgs = {inherit inputs pkgs;};
-    modules = [
-      # Darwin modules
-      ../darwin/work.nix
+  inputs.nix-darwin.lib.darwinSystem (shared.darwinSystemConfig
+    // {
+      modules = [
+        # Darwin modules
+        ../darwin/work.nix
 
-      # Home Manager module
-      inputs.home-manager.darwinModules.home-manager
-      {
-        # Home Manager setup
-        home-manager = {
-          extraSpecialArgs = {inherit inputs;};
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.adampie = {lib, ...}: {
-            home = {
-              stateVersion = "24.11";
-              homeDirectory = lib.mkForce "/Users/adampie";
-              username = "adampie";
-              packages = with pkgs; [
-                dive
-                unstable.claude-code
-              ];
+        # Home Manager module
+        inputs.home-manager.darwinModules.home-manager
+        {
+          # Home Manager setup
+          home-manager =
+            shared.homeManagerConfig
+            // {
+              users.adampie = import ../home-manager/work.nix;
             };
-          };
-        };
-      }
-    ];
-  }
+        }
+      ];
+    })
