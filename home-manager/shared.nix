9d29b9d08e2a1e3d24343328d9c7f56236f5d6d5
Adam Pietrzycki <adam@pietrzycki.com>
2025-05-17 22:48:29 +0100

    Enhance Home Manager configuration with SSH and Homebrew settings
    - Added SSH configuration to include Orbstack settings and specify IdentityAgent.
    - Configured Homebrew environment variables to improve security and control over updates.

--- home-manager/shared.nix
+++ home-manager/shared.nix
@@ -16,21 +16,36 @@
       jq
       yq
       wget
-      
+
       unstable.claude-code
     ];
-    
+
     file.".hushlogin".text = "";
+    file.".ssh/config" = {
+      text = ''
+        Include ~/.orbstack/ssh/config
+
+        Host *
+          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+    };
+    file.".homebrew/brew.env".text = ''
+      export HOMEBREW_NO_INSECURE_REDIRECT=1
+      export HOMEBREW_NO_ANALYTICS=1
+      export HOMEBREW_CASK_OPTS=--require-sha
+      export HOMEBREW_NO_AUTO_UPDATE=1
+      export HOMEBREW_NO_ENV_HINTS=1
+    '';
   };
 
   programs = {
     zsh.enable = true;
-    
+
     starship = {
       enable = true;
       enableZshIntegration = true;
     };
-    
+
     git = {
       enable = true;
       userName = "Adam Pietrzycki";
